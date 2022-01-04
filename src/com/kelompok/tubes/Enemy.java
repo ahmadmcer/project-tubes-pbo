fcb07d3c4fd45d0f90af9257a6308b27c98287a7
AhmadMCer <ahmadmcer@gmail.com>
2022-01-04 19:18:03 +0700

    fix: change enemy stats mechanism

--- src/com/kelompok/tubes/Enemy.java
+++ src/com/kelompok/tubes/Enemy.java
@@ -2,19 +2,11 @@ package com.kelompok.tubes;
 
 public class Enemy {
     protected String name;
-    private int health;
-    private int maxHealth;
-    private int attack;
-    private int defense;
-    private int level;
-
-    public Enemy(int health, int maxHealth, int attack, int defense, int level) {
-        this.health = health;
-        this.maxHealth = maxHealth;
-        this.attack = attack;
-        this.defense = defense;
-        this.level = level;
-    }
+    protected int health;
+    protected int maxHealth;
+    protected int attack;
+    protected int defense;
+    protected int level;
 
     public String getName() {
         return name;

afff81e9e9503b08fda365aa5c9a1a5472b93a39
AhmadMCer <ahmadmcer@gmail.com>
2022-01-04 21:36:24 +0700

    fix: change gameplay mechanism

--- src/com/kelompok/tubes/Minotaur.java
+++ src/com/kelompok/tubes/Minotaur.java
@@ -3,10 +3,10 @@ package com.kelompok.tubes;
 public class Minotaur extends Enemy{
     public Minotaur(int level) {
         this.name = "Minotaur";
-        this.health = 20 * level;
-        this.maxHealth = 20 * level;
-        this.attack = 5 * level;
-        this.defense = 10 * level;
+        this.health = 30 * level;
+        this.maxHealth = 30 * level;
+        this.attack = 10 * level;
+        this.defense = 5 * level;
         this.level = level;
     }
 }

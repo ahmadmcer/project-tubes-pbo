cc8463086d891858c4b1feeeb9bfc50f3b579c44
AhmadMCer <ahmadmcer@gmail.com>
2022-01-05 22:55:23 +0700

    checkpoint

--- src/com/kelompok/tubes/Main.java
+++ src/com/kelompok/tubes/Main.java
@@ -1,6 +1,5 @@
 package com.kelompok.tubes;
 
-import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 public class Main {
@@ -10,77 +9,55 @@ public class Main {
         Scanner scan = new Scanner(System.in); // Scanner
 
         // Initialization Variables
-        boolean input = true;
         boolean gameover = false; // For checking if is game over
         boolean canWalk = true; // For checking if is can walk or not
         boolean canRest = false; // For checking if is can rest or not
         boolean fight = false; // For checking if is fight or not
+        int gacha; // Gacha event (random)
         int code; // Code for input action
-        int gacha; // Gacha (random) events
 
-        // Welcome message
-        do {
-            System.out.println("Welcome to Dragon Slayer!");
-            System.out.println("1. Start");
-            System.out.println("2. Exit");
-            System.out.print("> ");
+        System.out.println("Welcome to Dragon Slayer!");
+        System.out.println("1. Start");
+        System.out.println("2. Exit");
+        System.out.print("> ");
+        code = scan.nextInt();
 
-            try {
-                code = scan.nextInt();
-                if (code == 1) {
-                    break;
-                } else if (code == 2) {
-                    System.exit(0);
-                } else {
-                    System.out.println("\n[ERROR] Input action not recognized, please try again!");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("\n[ERROR] Input action not recognized, please try again!");
-                scan.nextLine();
-            }
-        } while (true);
+        // For exit
+        if (code == 2) {
+            System.exit(0);
+        }
 
         // Initialization Player
         Player player = new Player(20, 20, 5, 5, 0, 8, 1);
 
-        // Game start
+        // Main game
         while (!gameover) {
-            // Walking
             while (canWalk) {
-                do {
-                    System.out.println("\nYou woke up.");
-                    System.out.println("[Your Action]");
-                    System.out.println("1. Walk");
-                    System.out.println("2. Show Stats");
-
-                    try {
-                        System.out.print("> ");
-                        code = scan.nextInt();
-                        break;
-                    } catch (Exception e) {
-                        System.out.println("\n[ERROR] Input action not recognized, please try again!");
-                        scan.nextLine();
-                    }
-                } while (true);
-
+                System.out.println("\nYou woke up.");
+                System.out.println("[Your Action]");
+                System.out.println("1. Walk");
+                System.out.println("2. Show Stats");
+                System.out.print("> ");
+                code = scan.nextInt();
 
                 switch (code) {
-                    case 1: // Walk
-                        gacha = random.nextInt(10); // Gacha for chance to meet enemy
+                    case 1:
+                        // Walk
+                        gacha = random.nextInt(10);
                         if (gacha > 2) {
                             // Initialization Enemy
                             Enemy enemy;
-                            gacha = random.nextInt(3); // Gacha for the chance of enemy types that appear
+                            gacha = random.nextInt(3);
                             switch (gacha) {
-                                case 0: // Slime
+                                case 0:
                                     gacha = random.nextInt(player.getLevel());
                                     enemy = new Slime(gacha + 1);
                                     break;
-                                case 1: // Goblin
+                                case 1:
                                     gacha = random.nextInt(player.getLevel());
                                     enemy = new Goblin(gacha + 1);
                                     break;
-                                case 2: // Minotaur
+                                case 2:
                                     gacha = random.nextInt(player.getLevel());
                                     enemy = new Minotaur(gacha + 2);
                                     break;
@@ -108,28 +85,18 @@ public class Main {
                                     throw new IllegalStateException("Unexpected value: " + gacha);
                             }
 
-                            // Message meets enemy
-                            do {
-                                System.out.println("\nYou cross paths with " + enemy.getName() + " Lv " + enemy.getLevel() + ".");
-                                System.out.println("[Your Action]");
-                                System.out.println("1. Fight");
-                                System.out.println("2. Run");
-
-                                try {
-                                    System.out.print("> ");
-                                    code = scan.nextInt();
-                                    break;
-                                } catch (Exception e) {
-                                    System.out.println("\n[ERROR] Input action not recognized, please try again!");
-                                    scan.nextLine();
-                                }
-                            } while (true);
+                            System.out.println("\nYou cross paths with " + enemy.getName() + " Lv " + enemy.getLevel() + ".");
+                            System.out.println("[Your Action]");
+                            System.out.println("1. Fight");
+                            System.out.println("2. Run");
+                            System.out.print("> ");
+                            code = scan.nextInt();
 
                             switch (code) {
-                                case 1: // Fight with enemy
+                                case 1:
                                     fight = true;
                                     break;
-                                case 2: // Try run to escape
+                                case 2:
                                     gacha = random.nextInt(2);
                                     if (gacha == 1) {
                                         fight = true;
@@ -144,38 +111,29 @@ public class Main {
 
                             while (fight) {
                                 // Player turn
-                                do {
-                                    System.out.print("\n[Player Lv. " + player.getLevel() + "]");
-                                    System.out.println("\t\t\t[" + enemy.getName() + " Lv. " + enemy.getLevel() + "]");
+                                System.out.print("\n[Player Lv. " + player.getLevel() + "]");
+                                System.out.println("\t\t\t[" + enemy.getName() + " Lv. " + enemy.getLevel() + "]");
 
-                                    System.out.print(" HP : " + player.getHealth() + "/" + player.getMaxHealth());
-                                    System.out.println("\t\t\t\t HP : " + enemy.getHealth() + "/" + enemy.getMaxHealth());
+                                System.out.print(" HP : " + player.getHealth() + "/" + player.getMaxHealth());
+                                System.out.println("\t\t\t\t HP : " + enemy.getHealth() + "/" + enemy.getMaxHealth());
 
-                                    System.out.print(" ATK: " + player.getAttack());
-                                    System.out.println("\t\t\t\t\t ATK: " + enemy.getAttack());
+                                System.out.print(" ATK: " + player.getAttack());
+                                System.out.println("\t\t\t\t\t ATK: " + enemy.getAttack());
 
-                                    System.out.print(" DEF: " + player.getDefense());
-                                    System.out.println("\t\t\t\t\t DEF: " + enemy.getDefense());
+                                System.out.print(" DEF: " + player.getDefense());
+                                System.out.println("\t\t\t\t\t DEF: " + enemy.getDefense());
 
-                                    System.out.println("\n[Your Action]");
-                                    System.out.println("1. Attack");
-                                    System.out.println("2. Defend");
-
-                                    try {
-                                        System.out.print("> ");
-                                        code = scan.nextInt();
-                                        break;
-                                    } catch (Exception e) {
-                                        System.out.println("\n[ERROR] Input action not recognized, please try again!");
-                                        scan.nextLine();
-                                    }
-                                } while (true);
+                                System.out.println("\n[Your Action]");
+                                System.out.println("1. Attack");
+                                System.out.println("2. Defend");
+                                System.out.print("> ");
+                                code = scan.nextInt();
 
                                 switch (code) {
-                                    case 1: // Player attack
+                                    case 1:
                                         System.out.print("\nYou attack the enemy, ");
                                         player.setDefend(false);
-                                        if (enemy.getDefend()) { // Enemy is defending
+                                        if (enemy.getDefend()) { // Enemy defending
                                             int hit = player.getAttack() - enemy.getDefense();
                                             if (hit > 0) {
                                                 enemy.onHit(hit);
@@ -183,12 +141,12 @@ public class Main {
                                             } else {
                                                 System.out.println("they didn't get hurt.");
                                             }
-                                        } else { // Enemy isn't defending
+                                        } else { // Enemy not defending
                                             enemy.onHit(player.getAttack());
                                             System.out.println("they lost " + player.getAttack() + " HP.");
                                         }
                                         break;
-                                    case 2: // Player defend
+                                    case 2:
                                         System.out.println("\nYou're defending.");
                                         player.setDefend(true);
                                         break;
@@ -196,9 +154,9 @@ public class Main {
 
                                 // Enemy turn
                                 gacha = random.nextInt(2);
-                                if (gacha == 1) { // Enemy attack
+                                if (gacha == 1) { // Enemy attacking
                                     enemy.setDefend(false);
-                                    if (player.getDefend()) { // Player is defending
+                                    if (player.getDefend()) { // Player
                                         int hit = enemy.getAttack() - player.getDefense();
                                         if (hit > 0) {
                                             player.onHit(hit);
@@ -206,16 +164,16 @@ public class Main {
                                         } else {
                                             System.out.println("Enemy is attacking, you didn't get hurt.");
                                         }
-                                    } else { // Player isn't defending
+                                    } else {
                                         player.onHit(enemy.getAttack());
                                         System.out.println("Enemy is attacking, you lost " + enemy.getAttack() + " HP.");
                                     }
-                                } else { // Enemy defend
+                                } else { // Enemy defending
                                     System.out.println("Enemy is defending.");
                                     enemy.setDefend(true);
                                 }
 
-                                // Player died
+                                // Player dead
                                 if (player.getHealth() <= 0) {
                                     System.out.println("\nYou died!");
                                     System.out.println("EXP: " + player.getExperience() + "/" + player.getMaxExperience());
@@ -225,16 +183,15 @@ public class Main {
                                     gameover = true;
                                 }
 
-                                // Enemy died
+                                // Player win fight
                                 if (enemy.getHealth() <= 0) {
                                     System.out.println("\nYou win fight!");
 
-                                    // Player get experience
                                     int experience = 8 * enemy.getLevel();
                                     player.addExperience(experience);
                                     System.out.println("You get " + experience + " EXP.");
 
-                                    while (player.getExperience() >= player.getMaxExperience()) { // Player leveling up
+                                    while (player.getExperience() >= player.getMaxExperience()) {
                                         System.out.println("\nYou leveled up!");
                                         player.addExperience(player.getMaxExperience() * -1);
                                         player.addMaxExperience(8);
@@ -243,28 +200,19 @@ public class Main {
                                         player.addMaxHealth(20);
 
                                         // Get +1 stats
-                                        do {
-                                            System.out.println("\nYou get +5 stats, please choose:");
-                                            System.out.println("1. Attack +5");
-                                            System.out.println("2. Defend +5");
-
-                                            try {
-                                                System.out.print("> ");
-                                                code = scan.nextInt();
-                                                break;
-                                            } catch (Exception e) {
-                                                System.out.println("\n[ERROR] Input action not recognized, please try again!");
-                                                scan.nextLine();
-                                            }
-                                        } while (true);
+                                        System.out.println("\nYou get +1 stats, please choose:");
+                                        System.out.println("1. Attack +1");
+                                        System.out.println("2. Defend +1");
+                                        System.out.print("> ");
+                                        code = scan.nextInt();
 
                                         // Choose stats to increase +1
                                         switch (code) {
                                             case 1:
-                                                player.addAttack(5);
+                                                player.addAttack(1);
                                                 break;
                                             case 2:
-                                                player.addDefense(5);
+                                                player.addDefense(1);
                                                 break;
                                         }
                                     }
@@ -280,7 +228,8 @@ public class Main {
                             break;
                         }
                         break;
-                    case 2: // Show stats
+                    case 2:
+                        // Show stats
                         System.out.println("\n[Player Stats]");
                         System.out.println(" HP : " + player.getHealth() + "/" + player.getMaxHealth());
                         System.out.println(" ATK: " + player.getAttack());
@@ -291,32 +240,26 @@ public class Main {
                 }
             }
 
-            // Resting
             while (canRest) {
-                do {
-                    System.out.println("\nYou're very tired and you can't keep going for tonight.");
-                    System.out.println("[Your Action]");
-                    System.out.println("1. Rest");
-                    System.out.println("2. Show Stats");
+                if (gameover) break;
 
-                    try {
-                        System.out.print("> ");
-                        code = scan.nextInt();
-                        break;
-                    } catch (Exception e) {
-                        System.out.println("\n[ERROR] Input action not recognized, please try again!");
-                        scan.nextLine();
-                    }
-                } while (true);
+                System.out.println("\nYou're very tired and you can't keep going for tonight.");
+                System.out.println("[Your Action]");
+                System.out.println("1. Rest");
+                System.out.println("2. Show Stats");
+                System.out.print("> ");
+                code = scan.nextInt();
 
                 switch (code) {
-                    case 1: // Rest
+                    case 1:
+                        // Rest
                         System.out.println("\nYou're now resting.");
                         player.setHealth(player.getMaxHealth());
                         canRest = false;
                         canWalk = true;
                         break;
-                    case 2: // Show stats
+                    case 2:
+                        // Show stats
                         System.out.println("\n[Player Stats]");
                         System.out.println(" HP : " + player.getHealth() + "/" + player.getMaxHealth());
                         System.out.println(" ATK: " + player.getAttack());
